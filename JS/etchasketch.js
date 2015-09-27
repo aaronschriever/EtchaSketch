76479e98b63336a30d1c5b014c54403a8d1067b6
Arnoo_s <aaron.schriever@gmail.com>
2015-09-27 13:50:00 +0930

    adding for gh-pages, add button dor clear

--- JS/etchasketch.js
+++ JS/etchasketch.js
@@ -1,25 +1,15 @@
 var number_of_squares = 960;
 var canvas_size = 960;
-$(document).ready(function(){
-make_squares(number_of_squares);
-$('.block').hover(function(){
 
-$(this).css('background-color', "#FF4136");
-$(this).addClass('red');
-
-	}/*,
-function(){
-		$(this).css('background-color', "#ccc");	
-		//$(this).addClass('block');
-	}*/);
-});
-
+function clear(){
+alert("it works!");
+	number_of_squares = prompt("how Many squares?");
+}
 
 function make_squares(_number_of_squares)
 {
 var size =  canvas_size / Math.sqrt(number_of_squares);
 
-//document.write(_number_of_squares);
 for (i = 0; i < _number_of_squares; i++)
 { 
 $("#container").append("<div class = \"block\"></div>");
@@ -31,3 +21,16 @@ $('.block').css('background-color', "#ccc");
 $('.block').css('margin', '1px');
 $('.block').css('float', 'left');
 }
+
+$(document).ready(function(){
+make_squares(number_of_squares);
+$('.block').hover(function(){
+
+$(this).css('background-color', "#FF4136");
+$(this).addClass('red');
+	});
+});
+
+
+
+
