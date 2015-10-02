3c7e3152e2d4f91857ae5ad59c1a456a4c271eb2
Arnoo_s <aaron.schriever@gmail.com>
2015-10-03 00:56:17 +0930

    add tint button and tint effect

--- JS/etchasketch.js
+++ JS/etchasketch.js
@@ -1,9 +1,30 @@
-var number_of_squares = 960;
-var canvas_size = 960;
+var number_of_squares = 600;
+var canvas_size = 600;
+var trail_type = 1;
+function default_boxes()
+{
+	change_boxes();
+	set_Color();
+}
+function tint_boxes()
+{
+change_boxes();
+lighten_squares();
+}
+function change_boxes(){
 
-function clear(){
-alert("it works!");
-	number_of_squares = prompt("how Many squares?");
+	number_of_squares = prompt("how Many squares per side?");
+	if (number_of_squares < 150){
+	number_of_squares = number_of_squares * number_of_squares;
+	$("#container").empty();
+	make_squares(number_of_squares);
+	
+}
+else
+{
+alert("please enter a number under 150");
+change_boxes();
+}
 }
 
 function make_squares(_number_of_squares)
@@ -15,22 +36,41 @@ for (i = 0; i < _number_of_squares; i++)
 $("#container").append("<div class = \"block\"></div>");
 
 }
-$('.block').css('width', size-2);
-$('.block').css('height', size-2);
+$('.block').css('width', size-1);
+$('.block').css('height', size-1);
 $('.block').css('background-color', "#ccc");
-$('.block').css('margin', '1px');
+$('.block').css('margin', '0.5px');
 $('.block').css('float', 'left');
+}
+function set_Color()
+{
+$('.block').mouseover(function(){
+var current_color = $(this).css("background-color");
+$(this).css('background-color', "#FF4136");
+$(this).addClass('red');
+
+	});
+	
+}
+
+function lighten_squares()
+{
+	$('.block').css('background-color', "#fff");
+	$("#container").css('background-color', "#000");
+$('.block').mouseover(function(){
+var current_color = $(this).css("opacity");
+	if (current_color > 0)
+	{
+		$(this).css('opacity',current_color - 0.1);
+
+	}});
+
 }
 
 $(document).ready(function(){
 make_squares(number_of_squares);
-$('.block').hover(function(){
-
-$(this).css('background-color', "#FF4136");
-$(this).addClass('red');
-	});
+set_Color();
 });
-
 
 
 
